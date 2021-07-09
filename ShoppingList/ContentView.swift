d4973bce5bb53ef77324bd2acef13f210f49be50
Fapse <fb-online@posteo.de>
2021-07-09 11:07:28 +0200

    Initial Commit

---
+++ ShoppingList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Fabian Braig on 09.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Fabian Braig on 09.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
